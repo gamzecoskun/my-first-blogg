8c8324b217d120bdb55aba1a2664f3336bddb129
gamzecoskun <11gamzecoskun@gmail.com>
2016-12-17 14:28:19 +0300

    Django Girls uygulamam, ilk commit

---
+++ blog/migrations/0002_auto_20161217_1410.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('baslik', models.CharField(max_length=200)),
+                ('yazi', models.TextField()),
+                ('yaratilma_tarihi', models.DateTimeField(default=django.utils.timezone.now)),
+                ('yayinlanma_tarihi', models.DateTimeField(blank=True, null=True)),
+                ('yazar', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='posts',
+            name='yazar',
+        ),
+        migrations.DeleteModel(
+            name='Posts',
+        ),
+    ]
